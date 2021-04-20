8e49b116824fb5e93faa14b4cddb0a0862550d5d
Milind Bhor <bhormilind@gmail.com>
2021-04-20 16:16:13 -0400

    New Fetch with Cancel Logic

--- src/app/common/util.ts
+++ src/app/common/util.ts
@@ -1,2 +1,27 @@
+import { Observable } from "rxjs";
+import { Course } from "../model/course";
+
+export function createHttpObservable(url: string): Observable<Course[]> {
+
+  return new Observable( subscriber => {
+
+    const controller = new AbortController();
+
+    const signal = controller.signal;
+
+    fetch(url, {signal})
+    .then( res => { return res.json()})
+    .then(body => {
+      subscriber.next(body);
+
+      subscriber.complete();
+    })
+    .catch( err => {
+      subscriber.error(err);
+    });
+
+    return () => controller.abort();
+  });
+}
 
 
